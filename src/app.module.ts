208761e5b4c97e6b81e4ae63cb60a38d627da16b
mateus-werneck <werneck.mateus@gmail.com>
2022-12-17 10:59:23 -0300

    Ajustes

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,6 +2,9 @@ import { getEnvPath } from '@Helpers/env.helper';
 import { RouteModule } from '@Routes/route.module';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { BasicStrategy } from '@Services/auth/strategies/basic.strategy';
+import { JwtAuthGuard } from '@Guards/authentication/jwt-auth.guard';
 import { AuthModule } from './Config/auth/auth.module';
 
 const envFilePath: string = getEnvPath(`${__dirname}/Config/envs`);
@@ -11,6 +14,7 @@ const envFilePath: string = getEnvPath(`${__dirname}/Config/envs`);
     AuthModule,
     ConfigModule.forRoot({ envFilePath, isGlobal: true }),
     RouteModule
-  ]
+  ],
+  providers: [{ provide: APP_GUARD, useClass: JwtAuthGuard }, BasicStrategy]
 })
 export class AppModule {}
